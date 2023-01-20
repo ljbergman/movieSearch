4d45abeae4ee5343fd3f47329126275d1c8162f9
Jonas Bergman <ljbergman@outlook.com>
2023-01-20 01:23:51 +0100

    More movies update 1

--- dist/moviedb_afetch.js
+++ dist/moviedb_afetch.js
@@ -1,7 +1,9 @@
 "use strict";
 let hostDomain = 'rapidapi.com';
 let akey = 'p1564fajsnef80add6fcd';
+let mNum = 0;
 const button = document.querySelector("#button");
+const button2 = document.querySelector("#button2");
 let searchField = document.querySelector("#searchField");
 let searchResults = document.querySelector("#searchResults");
 let searchHits = document.querySelector("#searchHits");
@@ -9,6 +11,7 @@ let searchResults2 = document.querySelector("#searchResults2");
 let searchResults3 = document.querySelector("#searchResults3");
 let moviePoster = document.querySelector("#movieposter");
 let searchResults4 = document.querySelector("#searchResults4");
+let nextResult = document.querySelector("#nextResult");
 let loadingGIF = document.querySelector("#loading");
 async function getMovie(searchInput) {
     const options = {
@@ -25,9 +28,17 @@ async function getMovie(searchInput) {
 const movieObject = {
     movie: []
 };
+function showMovie(mNum) {
+    let mNum2 = mNum + 1;
+    searchResults2.innerHTML = "<b>#" + mNum2 + " " + movieObject.movie[mNum].title + "</b><br>" + movieObject.movie[mNum].type + " from " + movieObject.movie[mNum].year;
+    moviePoster.src = movieObject.movie[mNum].poster;
+    searchResults4.innerHTML = `<a href="https://www.imdb.com/title/${movieObject.movie[mNum].imdbID}" target="_blank">IMDB Link</a>`;
+}
 button.addEventListener('click', function (event) {
     event.preventDefault();
     loadingGIF.style.display = "block";
+    movieObject.movie = [];
+    mNum = 0;
     getMovie(searchField.value).then((data) => {
         setTimeout(function () {
             loadingGIF.style.display = "none";
@@ -35,11 +46,11 @@ button.addEventListener('click', function (event) {
             if (hits === undefined) {
                 hits = "0";
             }
+            if (parseInt(hits) > 10) {
+                hits = "10";
+            }
             let showHits = hits + " hits";
             searchHits.innerHTML = showHits;
-            searchResults2.innerHTML = "<b>#1 " + data.Search[0].Title + "</b><br>" + data.Search[0].Type + " from " + data.Search[0].Year;
-            moviePoster.src = data.Search[0].Poster;
-            searchResults4.innerHTML = `<a href="https://www.imdb.com/title/${data.Search[0].imdbID}" target="_blank">IMDB Link</a>`;
             let newMovie;
             for (let key in data.Search) {
                 newMovie = {
@@ -51,6 +62,16 @@ button.addEventListener('click', function (event) {
                 };
                 movieObject.movie.push(newMovie);
             }
+            showMovie(mNum);
+            nextResult.style.display = "block";
         }, 1000);
     });
+});
+button2.addEventListener('click', function (event) {
+    event.preventDefault();
+    mNum = mNum + 1;
+    if (mNum == movieObject.movie.length) {
+        mNum = 0;
+    }
+    showMovie(mNum);
 });
